3ec4c8150e599163a5837ecfad03d931bf96a9d2
Oscar Amarilla <oscaramarillacaceres@gmail.com>
2026-03-07 12:38:39 -0300

    fix MoviAula page vacia

---
+++ app/MoviAula/page.tsx
@@ -0,0 +1,8 @@
+export default function MoviAulaPage() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-3xl font-bold">MoviAula</h1>
+      <p className="text-zinc-500 mt-2">Próximamente...</p>
+    </div>
+  );
+}
