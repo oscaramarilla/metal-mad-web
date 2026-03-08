b1003ebee8cb1e494288d5b056df8c43f7697abe
Oscar Amarilla <oscaramarillacaceres@gmail.com>
2026-03-08 16:38:07 -0300

    vercel solucion next conf ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // Peligroso para equipos grandes, perfecto para nosotros ahora.
-    // Le dice a Vercel que ignore los errores de tipos y publique.
-    ignoreBuildErrors: true,
-  },
   eslint: {
-    // Le dice a Vercel que ignore las advertencias de comillas y texto.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
